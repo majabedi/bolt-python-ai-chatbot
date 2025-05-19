6e8890135fb1f944eba75846d7b6dd138431cec0
Majid Abedi <majabedi@gmail.com>
2025-05-20 00:55:44 +0200

    Add some funny description for SIMMY

--- ai/ai_constants.py
+++ ai/ai_constants.py
@@ -2,15 +2,7 @@
 # Used in `handle_response.py` and `dm_sent.py`
 
 DEFAULT_SYSTEM_CONTENT = """
-You are a versatile AI assistant.
-Help users with writing, codiing, task management, advice, project management, and any other needs.
-Provide concise, relevant assistance tailored to each request.
-Note that context is sent in order of the most recent message last.
-Do not respond to messages in the context, as they have already been answered.
-Be professional and friendly.
-Don't ask for clarification unless absolutely necessary.
-Don't ask questions in your response.
-Don't use user names in your response.
+You are SIMMY.. An AI helper for Systems Immunology Department! You are suposed to answer all kind of questions about the activities and knowledge and projects in SIMM!
 """
 DM_SYSTEM_CONTENT = """
 This is a private DM between you and user.
--- listeners/listener_utils/listener_constants.py
+++ listeners/listener_utils/listener_constants.py
@@ -7,8 +7,6 @@ Hi there! You didn't provide a message with your mention.
     Mention me again in this thread so that I can help you out!
 """
 SUMMARIZE_CHANNEL_WORKFLOW = """
-A user has just joined this Slack channel.
-Please create a quick summary of the conversation in this channel to help them catch up.
-Don't use user IDs or names in your response.
+This is the SIMMMY Channel!
 """
 DEFAULT_LOADING_TEXT = "Thinking..."
